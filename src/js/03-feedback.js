f47c6002e2ed70fb22dca5a4ab5dee35158f7ab8
SergeyZhydkov <134008114+SergeyZhydkov@users.noreply.github.com>
2023-10-14 23:41:28 +0300

    продовження завдання

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,11 @@
-
+import throttle from 'lodash.throttle';
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', inputValue);
+function inputValue(evt) {
+  evt.preventDefault();
+}
+let email = input.email;
+localStorage.setItem('feedback-form-state', JSON.stringify(email));
+let message = input.message;
+localStorage.setItem('feedback-form-state', JSON.stringify(message));
+console.log(email);
