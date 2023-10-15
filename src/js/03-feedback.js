c3a4402600f1557776f0857dc005801bf5393130
SergeyZhydkov <134008114+SergeyZhydkov@users.noreply.github.com>
2023-10-15 22:39:57 +0300

    доопрацювання

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,11 +1,41 @@
 import throttle from 'lodash.throttle';
-const form = document.querySelector('.feedback-form');
-form.addEventListener('input', inputValue);
+const STORAGE_KEY = 'feedback-form-state';
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('.email-content'),
+  message: document.querySelector('.message-content'),
+};
+let dataSet = {};
+refs.form.addEventListener('input', throttle(inputValue, 500));
 function inputValue(evt) {
   evt.preventDefault();
+  dataSet[evt.target.name] = evt.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(dataSet));
 }
-let email = input.email;
-localStorage.setItem('feedback-form-state', JSON.stringify(email));
-let message = input.message;
-localStorage.setItem('feedback-form-state', JSON.stringify(message));
-console.log(email);
+
+function storageData() {
+  const saveData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  console.log(saveData);
+  if (saveData) {
+    refs.email.value = saveData.email ?? [];
+    dataSet.email = saveData.email;
+    refs.message.value = saveData.message ?? [];
+    dataSet.message = saveData.message;
+  }
+}
+storageData();
+
+refs.form.addEventListener('submit', onFormSubmit);
+function onFormSubmit(evt) {
+  if (refs.email.value === '' || refs.message.value === '') {
+    alert('Please enter all fields!');
+    return;
+  } else {
+    console.log(dataSet);
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+    dataSet = {};
+    refs.email = '';
+    refs.message = '';
+  }
+}
